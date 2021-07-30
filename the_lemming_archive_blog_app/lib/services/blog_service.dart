63d28246e17f0ce719ef33a4b84803f30655c330
Pedro Celis <34390914+pcelis19@users.noreply.github.com>
2021-07-30 10:25:51 -0700

    Update blog_service.dart

--- the_lemming_archive_blog_app/lib/services/blog_service.dart
+++ the_lemming_archive_blog_app/lib/services/blog_service.dart
@@ -1,4 +1,14 @@
+import 'package:webfeed/webfeed.dart';
+
+
 class BlogService {
   static const String lemmingArchiveRssFeed =
       'https://lemmingarchive.blogspot.com/feeds/posts/default?alt=rss';
+  Future<Blog> get blog{
+    RssFeed rssFeed = Rss
+  }
+}
+
+class Blog {
+  final RssFeed rssFeed;
 }
